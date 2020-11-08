0785b7ea79f22e443c96a77d38750f3f92c70c11
Ramón Calvo <ramon@noctrog.xyz>
2020-11-08 17:29:27 +0100

    Back to the dark side

---
+++ config/.config/emacs/init.el
@@ -0,0 +1,114 @@
+;; hide GUI
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; disable bell
+(setq ring-bell-function 'ignore)
+
+;; Line numbering
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+;; font size
+(set-face-attribute 'default nil :height 120)
+
+;; Line highlighting
+(hl-line-mode 1)
+
+;; Straight package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; packages
+(straight-use-package 'evil)
+(straight-use-package 'evil-snipe)
+(straight-use-package 'evil-magit)
+(straight-use-package 'helm)
+(straight-use-package 'magit)
+(straight-use-package 'projectile)
+(straight-use-package 'dashboard)
+(straight-use-package 'which-key)
+(straight-use-package 'all-the-icons)
+(straight-use-package 'page-break-lines)
+(straight-use-package 'telephone-line)
+(straight-use-package 'doom-themes)
+
+;; evil mode
+(require 'evil)
+(evil-mode 1)
+(evil-snipe-mode 1)
+
+;; helm mode
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
+(global-set-key (kbd "C-x r b") 'helm-bookmarks) ;; Bookmarks menu
+(global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
+(global-set-key (kbd "M-c") 'helm-calcul-expression) ;; Use Helm for calculations
+(global-set-key (kbd "C-s") 'helm-occur)  ;; Replaces the default isearch keybinding
+(global-set-key (kbd "C-h a") 'helm-apropos)  ;; Helmized apropos interface
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Show kill ring, pick something to pastelm-mode 1)
+
+;; which key
+(require 'which-key)
+(which-key-mode 1)
+
+;; dashboard
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+(setq dashboard-startup-banner 'logo)
+(setq dashboard-center-content t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(setq dashboard-set-init-info t)
+
+;; modeline
+;; for telephone-line configuration needs to be before (telephhone-line-mode 1)
+(require 'telephone-line)
+(setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+      telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+      telephone-line-primary-right-separator 'telephone-line-cubed-right
+      telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+(setq telephone-line-height 24)
+(setq telephone-line-lhs
+      '((evil   . (telephone-line-evil-tag-segment))
+        (accent . (telephone-line-vc-segment
+                   telephone-line-erc-modified-channels-segment
+                   telephone-line-process-segment))
+        (nil    . (telephone-line-minor-mode-segment
+                   telephone-line-buffer-segment))))
+(setq telephone-line-rhs
+      '((nil    . (telephone-line-misc-info-segment))
+        (accent . (telephone-line-major-mode-segment))
+        (evil   . (telephone-line-airline-position-segment))))
+(telephone-line-mode 1)
+
+;; doom themes
+(load-theme 'doom-one t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "cae81b048b8bccb7308cdcb4a91e085b3c959401e74a0f125e7c5b173b916bf9" "8e959d5a6771b4d1e2177263e1c1e62c62c0f848b265e9db46f18754ea1c1998" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
