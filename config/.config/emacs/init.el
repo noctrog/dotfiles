9fb21e164fdc29376b0594a5adcc85d1affdf348
Ramón Calvo González <ramon@ramoncalvo.com>
2020-11-20 10:33:13 +0100

    buffer name before minor modes

--- config/.config/emacs/init.el
+++ config/.config/emacs/init.el
@@ -179,8 +179,8 @@
         (accent . (telephone-line-vc-segment
                    telephone-line-erc-modified-channels-segment
                    telephone-line-process-segment))
-        (nil    . (telephone-line-minor-mode-segment
-                   telephone-line-buffer-segment))))
+        (nil    . (telephone-line-buffer-segment
+		   telephone-line-minor-mode-segment))))
 (setq telephone-line-rhs
       '((nil    . (telephone-line-misc-info-segment))
         (accent . (telephone-line-major-mode-segment))
