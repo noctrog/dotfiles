4f587de09287ba611f81cbddbcb8037006fd53e7
Ramón Calvo González <ramon@ramoncalvo.com>
2020-11-28 21:32:41 +0100

    Dashboard show projects

--- config/.config/emacs/init.el
+++ config/.config/emacs/init.el
@@ -160,8 +160,13 @@
 (setq dashboard-set-heading-icons t)
 (setq dashboard-set-file-icons t)
 (setq dashboard-set-init-info t)
+(setq dashboard-projects-backend 'projectile)
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
 ;; avy
 (avy-setup-default)
 
