702a93d6c98c5df51d7b5d1f07e906329a39d32d
Ramon Calvo Gonzalez <ramon@ramoncalvo.com>
2021-08-16 11:19:53 +0200

    Telescope config

--- neovim/.config/nvim/lua/init.lua
+++ neovim/.config/nvim/lua/init.lua
@@ -1,5 +1,55 @@
 -- Telescope
-require('telescope').setup()
+local tele_actions = require('telescope.actions')
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      n = {
+	      ["q"] = tele_actions.close
+      },
+    },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    prompt_prefix = "> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+    },
+    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    winblend = 0,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+    use_less = true,
+    path_display = {},
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+  }
+}
 require('telescope').load_extension('fzy_native')
 -- require('telescope').load_extension('cheat')
 
@@ -141,9 +191,11 @@ wk.register({
 		s = { "<cmd>PackerSync<cr>", "Packer Sync" },
 	},
 	-- Telescope
+	T = { "<cmd>Telescope<cr>", "Telescope"},
 	f = {
 		name = "file", -- optional group name
-		f = { "<cmd>Telescope find_files<cr>", "Find File" },   -- create a binding with label
+		f = { "<cmd>Telescope find_files<cr>", "Find File" },
+		F = { "<cmd>Telescope file_browser<cr>", "File Broswer" },
 		g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },    -- live grep
 		b = { "<cmd>Telescope buffers<cr>", "Switch buffers" }, -- switch buffer
 		h = { "<cmd>Telescope help_tags<cr>", "Help tags"},
