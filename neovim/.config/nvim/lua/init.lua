5db622ce9fcdd93e8c2e7867ea843864913d1022
Ramon Calvo Gonzalez <ramon@ramoncalvo.com>
2021-08-25 15:28:02 +0200

    Additional neorg settings

--- neovim/.config/nvim/lua/init.lua
+++ neovim/.config/nvim/lua/init.lua
@@ -262,14 +262,21 @@ require('neorg').setup {
     -- Tell Neorg what modules to load
     load = {
         ["core.defaults"] = {}, -- Load all the default modules
+	["core.keybinds"] = { -- Configure core.keybinds
+		config = {
+			default_keybinds = true, -- Generate the default keybinds
+			neorg_leader = "<Leader>n" -- This is the default if unspecified
+		}
+	},
         ["core.norg.concealer"] = {}, -- Allows for use of icons
         ["core.norg.dirman"] = { -- Manage your directories with Neorg
             config = {
                 workspaces = {
-                    my_workspace = "~/neorg"
+                    my_workspace = "~/Sync/neorg"
                 }
             }
-        }
+        },
+	["core.integrations.telescope"] = {}, -- Enable the telescope module
     },
 }
 
@@ -282,3 +289,5 @@ vim.g.NERDCreateDefaultMappings = 1
 vim.g.NERDSpaceDelims = 1
 vim.g.NERDCommentEmptyLines = 1
 vim.g.NERDCompactSexyComs = 1
+
+return M
