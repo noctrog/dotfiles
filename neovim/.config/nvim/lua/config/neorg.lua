4f70a654aed44834b4a0eed224fbec7aa839b143
Ramon Calvo Gonzalez <ramon@ramoncalvo.com>
2021-09-12 16:04:43 +0200

    Neovim refactor

--- neovim/.config/nvim/lua/config/neorg.lua
+++ neovim/.config/nvim/lua/config/neorg.lua
@@ -1,21 +1,31 @@
 require('neorg').setup {
-    -- Tell Neorg what modules to load
-    load = {
-        ["core.defaults"] = {}, -- Load all the default modules
-	["core.keybinds"] = { -- Configure core.keybinds
-		config = {
-			default_keybinds = true, -- Generate the default keybinds
-			neorg_leader = "<Leader>n" -- This is the default if unspecified
-		}
-	},
-        ["core.norg.concealer"] = {}, -- Allows for use of icons
-        ["core.norg.dirman"] = { -- Manage your directories with Neorg
-            config = {
-                workspaces = {
-                    my_workspace = "~/Sync/neorg"
+        -- Tell Neorg what modules to load
+        load = {
+                -- Load all the default modules
+                ["core.defaults"] = {},
+                -- Configure the keybinds
+                ["core.keybinds"] = {
+                        config = {
+                                default_keybinds = true, -- Generate the default keybinds
+                                neorg_leader = "<Leader>n" -- This is the default if unspecified
+                        }
+                },
+                ["core.norg.concealer"] = {}, -- Allows for use of icons
+                -- Manage Neorg directories
+                ["core.norg.dirman"] = {
+                        config = {
+                                workspaces = {
+                                        my_workspace = "~/Sync/neorg"
+                                }
+                        }
+                },
+                -- Enable the Telescope module
+                ["core.integrations.telescope"] = {},
+                -- Enable the completion module
+                ["core.norg.completion"] = {
+                        config = {
+                                engine = "nvim-cmp"
+                        }
                 }
-            }
         },
-	["core.integrations.telescope"] = {}, -- Enable the telescope module
-    },
 }
