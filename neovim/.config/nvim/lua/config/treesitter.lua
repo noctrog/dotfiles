012801f0f96d7faa5ceb227b3af26e8ebc3b4be7
Ramón Calvo <noctrog@gmail.com>
2021-11-24 11:13:20 +0100

    nvim: treesitter incremental selection

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -81,5 +81,6 @@ require('nvim-treesitter.configs').setup {
 	    },
     },
   },
+  incremental_selection = { enable = true, keymaps = { init_selection = '<CR>', scope_incremental = '<CR>', node_incremental = '<TAB>', node_decremental = '<S-TAB>', }, },
 }
 
