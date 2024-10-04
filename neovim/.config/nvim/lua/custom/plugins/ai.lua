e7e0a166f1ec661b5774c9c710f36839501cb0da
Ramón Calvo González <ramon@ramoncalvo.com>
2024-10-04 11:42:28 +0200

    nvim: nuke and start from kickstart

---
+++ neovim/.config/nvim/lua/custom/plugins/ai.lua
@@ -0,0 +1,23 @@
+-- Provide similar functionality as Cursor. AI powered code assistance
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  build = 'make',
+  opts = {
+    -- add any opts here
+  },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
--- neovim/.config/nvim/lua/gui.lua
+++
@@ -1,9 +0,0 @@
--- Gruvbox colorscheme
-vim.o.termguicolors = true -- enable 24-bit RGB colors
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
--- Status line
-require('config.statusline')
-
-require('ibl').setup{ }
