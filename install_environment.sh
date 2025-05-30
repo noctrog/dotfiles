0e76d29da4c1da16a2782896f6a58196ce7baeb6
CALVO GONZALEZ Ramon <calvogon@mlg007.unige.ch>
2025-05-30 21:06:49 +0200

    added cargo programs

--- install_environment.sh
+++ install_environment.sh
@@ -58,7 +58,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Install rust programs
 . $HOME/.cargo/env
-cargo install du-dust zoxide zellij cargo-cache --locked
+cargo install du-dust zoxide zellij cargo-cache cargo-update viu --locked
 cargo install --locked --features clipboard broot
 cargo install --git https://github.com/RaphGL/Tuckr.git
 cargo cache --autoclean
